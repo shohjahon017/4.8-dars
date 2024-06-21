adcce548d047fabe45def1965a8d07a8622ee43c
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-06-21 10:39:15 +0500

    ..

--- index.js
+++ index.js
@@ -1,1 +1,84 @@
-let arr = [1, 2, 3, 5, 6, 9];
+//Asosiy metodlarga oid masalar
+
+//1-masala
+
+// let arr = [1, 2, 3, 5, 6, 9];
+// function lengthElement(arr) {
+//   return arr.length;
+// }
+// console.log(lengthElement(arr));
+
+//2-masala
+// let arr = [1, 2, 3, 5, 6, 9];
+// function toStringElement(arr) {
+//   let res = arr.toString();
+//   return res;
+// }
+// console.log(toStringElement(arr));
+
+//3-masala
+// let arr = [1, 2, 3, 5, 6, 9];
+// function joinElement(arr) {
+//   let res = arr.join(", ");
+//   return res;
+// }
+// console.log(joinElement(arr));
+
+//4-masala
+// let arr = [1, 2, 3, 4, 8, 9, 10];
+// function addElement(arr, element) {
+//   arr.push(element);
+//   return arr;
+// }
+// console.log(addElement(arr, 12));
+
+//5-masala
+// let arr = [5, 3, 9, 15, 6, 77, 55];
+// function removeElement(arr) {
+//   arr.pop();
+//   return arr;
+// }
+// console.log(removeElement(arr));
+
+//6-masala
+// let arr = [44, 52, 6, 5, 88, 9];
+// function removefirstElement(arr) {
+//   arr.shift();
+//   return arr;
+// }
+
+// console.log(removefirstElement(arr));
+
+//7-masala
+// let arr = [2, 5, 6, 8, 3, 4, 7];
+// function addFirstElement(arr, element) {
+//   arr.unshift(element);
+//   return arr;
+// }
+// console.log(addFirstElement(arr, 1));
+
+//8-masala
+
+// let arr1 = [25, 3, 6, 5, 6, 9];
+// let arr2 = [25, 4, 56, 8, 63, 45];
+// function unifyTwoelement(arr1, arr2) {
+//   let res = arr1.concat(arr2);
+//   return res;
+// }
+// console.log(unifyTwoelement(arr1, arr2));
+
+//9-masala
+// let arr = [2, 6, 8, 9, 4, 0];
+// function spliceElement(arr) {
+//   arr.splice(1, 3, 5, 10);
+//   return arr;
+// }
+// console.log(spliceElement(arr));
+
+//10-masala
+// let arr = [5, 6, 8, 99, 4, 7];
+// function sliceElement(arr) {
+//   let res = arr.slice(1, 4);
+//   return res;
+// }
+// console.log(sliceElement(arr));
